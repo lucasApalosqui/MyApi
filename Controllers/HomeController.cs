fffc05a7ba235919d5c268360bec388371c53056
LucasApalosqui <lucas@apalosqui@gmail.com>
2023-12-04 16:42:04 -0300

    :sparkles: feat: GetById, Update and Delete Todo

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -22,5 +22,47 @@ namespace MinhaApi.Controllers
             return todo;
             
         }
+
+        [HttpGet("/home/{id:int}")]
+        public TodoModel GetById([FromServices] AppDbContext context, [FromRoute] int id)
+        {
+            return context.Todos.FirstOrDefault(x => x.Id == id);
+        }
+
+        [HttpPut("/home/{id:int}")]
+        public TodoModel Put([FromServices] AppDbContext context, [FromRoute] int id, [FromBody] TodoModel todo)
+        {
+            var model = context.Todos.FirstOrDefault(x => x.Id == id);
+            if (model == null)
+            {
+                return null;
+            }
+
+            model.Title = todo.Title;
+            model.Done = todo.Done;
+            
+            context.Update(model);
+            context.SaveChanges();
+
+            return model;
+        }
+
+        [HttpDelete("/home/{id:int}")]
+        public string Delete([FromServices] AppDbContext context, [FromRoute] int id)
+        {
+            var model = context.Todos.FirstOrDefault(x => x.Id == id);
+            if (model == null)
+            {
+                return "Tarefa não encontrada";
+            }
+
+            context.Todos.Remove(model);
+            context.SaveChanges();
+
+            return "Tarefa excluida com sucesso!";
+        }
+
+
+
     }
 }
