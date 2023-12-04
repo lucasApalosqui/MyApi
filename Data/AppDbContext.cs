930868ebebaeb1fae9f2e537d3c54b1b21796f63
LucasApalosqui <lucas@apalosqui@gmail.com>
2023-12-04 15:35:10 -0300

    :sparkles: feat(fluentBlog): DataContext Config and Generate DB

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MinhaApi.Models;
+
+namespace MinhaApi.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<TodoModel> Todos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder.UseSqlite("DataSource=app.db;Cache=Shared");
+        
+    }
+}
