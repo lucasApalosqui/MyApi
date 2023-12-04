c1ae89616cb27c34bb8967a0ec2419ed7ef863c5
LucasApalosqui <lucas@apalosqui@gmail.com>
2023-12-04 18:37:07 -0300

    :sparkles: feat(blogAspnet): Controllers First Configuration

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BlogAspNet.Controllers
+{
+    [ApiController]
+    [Route("")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
