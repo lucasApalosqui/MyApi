abb27af58227522d1e075a8fb333e81443c9441c
LucasApalosqui <lucas@apalosqui@gmail.com>
2023-12-04 15:56:56 -0300

    :sparkles: feat: Controller Configuration

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MinhaApi.Controllers
+{
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet("/home")]
+        public string Get()
+        {
+            return "Hello World";
+        }
+    }
+}
