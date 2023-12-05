e2e42050d4681df40263915123b800eb715adaa1
LucasApalosqui <lucas@apalosqui@gmail.com>
2023-12-05 13:30:58 -0300

    :sparkles: feat(blogAspnet): Validation Data

--- ViewModels/EditorCategoryViewModel.cs
+++ ViewModels/EditorCategoryViewModel.cs
@@ -1,8 +1,14 @@
-﻿namespace BlogAspNet.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlogAspNet.ViewModels
 {
     public class EditorCategoryViewModel
     {
+        [Required(ErrorMessage = "Nome é obrigatório")]
+        [StringLength(40, MinimumLength = 3, ErrorMessage = "Este campo deve conter entre 3 e 40 caracteres!")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "Slug é obrigatório")]
+        [StringLength(40, MinimumLength = 3, ErrorMessage = "Este campo deve conter entre 3 e 40 caracteres!")]
         public string  Slug { get; set; }
     }
 }
