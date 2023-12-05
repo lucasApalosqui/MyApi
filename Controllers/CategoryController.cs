a4ba07e918c602b88c9a4e4e9bb514a6cc37490b
LucasApalosqui <lucas@apalosqui@gmail.com>
2023-12-05 12:21:55 -0300

    :sparkles: feat(blogAspnet): Category Controller and Version Controll

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,23 @@
+﻿using BlogAspNet.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogAspNet.Controllers
+{
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        [HttpGet("v1/categories")]
+        public IActionResult Get([FromServices] DataContext context)
+        {
+            var categories = context.Categories.ToList();
+            return Ok(categories);
+        }
+
+        [HttpGet("v2/categories")]
+        public IActionResult Get2([FromServices] DataContext context)
+        {
+            var categories = context.Categories.ToList();
+            return Ok(categories);
+        }
+    }
+}
