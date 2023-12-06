8c659d722675f206edc56b155148a0a1a17ecd43
LucasApalosqui <lucas@apalosqui@gmail.com>
2023-12-06 11:56:44 -0300

    :sparkles: feat(blogAspnet): First Configuration token and jwt
    Install packages microsoft.aspnet.authentication, microsoft.apnet.authentication.jwtbearer

---
+++ Configuration.cs
@@ -0,0 +1,7 @@
+﻿namespace BlogAspNet
+{
+    public static class Configuration
+    {
+        public static string JwtKey { get; set; } = "ZGFmNDQyOGYtNjJhOS00YmYxLWEyMWYtMzA2OGZiOTNhYWM2=";
+    }
+}
---
+++ Services/TokenService.cs
@@ -0,0 +1,29 @@
+﻿using BlogAspNet.Models;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+
+namespace BlogAspNet.Services
+{
+    public class TokenService
+    {
+        public string GenerateToken(User user)
+        {
+            // criar instancia do token handler que gera o token
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            // Pega a chave 
+            var key = Encoding.ASCII.GetBytes(Configuration.JwtKey);
+
+            // Cria uma especificação do token
+            var tokenDescriptor = new SecurityTokenDescriptor();
+
+            // Cria o token com as especificações selecionadas
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            // converte tudo em uma string
+            return tokenHandler.WriteToken(token);
+
+        }
+    }
+}
