57d8483ee1ffc2eeff2aeb7b81a4dfab69ede329
LucasApalosqui <lucas@apalosqui@gmail.com>
2023-12-06 12:38:44 -0300

    :sparkles: feat(blogAspnet): Dependency Injection TokenService

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,19 @@
+﻿using BlogAspNet.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogAspNet.Controllers
+{
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+
+        [HttpPost("v1/login")]
+        public IActionResult login([FromServices] TokenService tokenService)
+        {
+            var token = tokenService.GenerateToken(null);
+
+            return Ok(token);
+        }
+
+    }
+}
