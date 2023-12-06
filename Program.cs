91aede6d10fd7c4c0c52e36e5d8bed44666c51ee
LucasApalosqui <lucas@apalosqui@gmail.com>
2023-12-06 12:53:20 -0300

    :sparkles: feat(blogAspnet): AddScoped, AddTransient, AddSingleton

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using BlogAspNet.Data;
+using BlogAspNet.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers()
@@ -7,6 +8,10 @@ builder.Services.AddControllers()
         options.SuppressModelStateInvalidFilter = true;
     });
 builder.Services.AddDbContext<DataContext>();
+
+builder.Services.AddTransient<TokenService>(); // Sempre criar um novo tokenService
+//builder.Services.AddScoped(); // Reaproveita o TokenService se está na mesma Requisição
+//builder.Services.AddSingleton(); // Utiliza o mesmo tokenService até que a aplic~ção seja parada
 var app = builder.Build();
 
 app.MapControllers();
