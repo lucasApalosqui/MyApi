4e3561214335285cb78464e04a500136f0e49540
LucasApalosqui <lucas@apalosqui@gmail.com>
2023-12-06 12:07:28 -0300

    :sparkles: feat(blogAspnet): Configuration tokenDescriptor

--- Services/TokenService.cs
+++ Services/TokenService.cs
@@ -16,7 +16,13 @@ namespace BlogAspNet.Services
             var key = Encoding.ASCII.GetBytes(Configuration.JwtKey);
 
             // Cria uma especificação do token
-            var tokenDescriptor = new SecurityTokenDescriptor();
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Expires = DateTime.UtcNow.AddHours(8),
+                SigningCredentials = new SigningCredentials( 
+                    new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature),
+
+            };
 
             // Cria o token com as especificações selecionadas
             var token = tokenHandler.CreateToken(tokenDescriptor);
