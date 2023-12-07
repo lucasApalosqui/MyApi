449920019fbfe96c008865ddf1602aa877c0086d
LucasApalosqui <lucas@apalosqui@gmail.com>
2023-12-07 13:14:56 -0300

    :sparkles: feat(blogAspnet): User Login

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -54,14 +54,34 @@ namespace BlogAspNet.Controllers
         }
 
         [HttpPost("v1/accounts/login")]
-        public IActionResult login([FromServices] TokenService tokenService)
+        public async Task<IActionResult> login([FromBody] LoginViewModel model, [FromServices] TokenService tokenService, [FromServices] DataContext context)
         {
-            var token = tokenService.GenerateToken(null);
+            if (!ModelState.IsValid)
+                return BadRequest(new ResultViewModel<string>(ModelState.GetErrors()));
 
-            return Ok(token);
-        }
 
-       
+            var user = await context.Users
+                .AsNoTracking()
+                .Include(x => x.Roles)
+                .FirstOrDefaultAsync(x => x.Email == model.Email);
+
+            if (user == null)
+                return StatusCode(401, new ResultViewModel<string>("Usuário ou senha inválidos"));
+
+            if(!PasswordHasher.Verify(user.PasswordHash, model.Password))
+                return StatusCode(401, new ResultViewModel<string>("Usuário ou senha inválidos"));
+
+            try
+            {
+                var token = tokenService.GenerateToken(user);
+                return Ok(new ResultViewModel<string>(token, null));
+            }
+            catch
+            {
+                return StatusCode(500, new ResultViewModel<string>("05X86 - Falha interna no servidor"));
+            }
+
+        }  
 
     }
 }
---
+++ ViewModels/LoginViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlogAspNet.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Informe o Email!")]
+        [EmailAddress(ErrorMessage = "Email Inválido!")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Informe a senha!")]
+        public string Password { get; set; }
+    }
+}
