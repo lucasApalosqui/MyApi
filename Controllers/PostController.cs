c66eb8d6ec2e987624ff55b6441f548e56b36b09
LucasApalosqui <lucas@apalosqui@gmail.com>
2023-12-08 16:36:58 -0300

    :sparkles: feat(performance): List Posts with Author and Category

---
+++ Controllers/PostController.cs
@@ -0,0 +1,34 @@
+﻿using BlogAspNet.Data;
+using BlogAspNet.Models;
+using BlogAspNet.ViewModels;
+using BlogAspNet.ViewModels.Posts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogAspNet.Controllers
+{
+    [ApiController]
+    public class PostController : ControllerBase
+    {
+        [HttpGet("v1/posts")]
+        public async Task<IActionResult> GetAsync([FromServices] DataContext context)
+        {
+            var posts = await context.Posts
+                .AsNoTracking()
+                .Include(x => x.Category)
+                .Include(x => x.Author)
+                .Select(x => new ListPostsViewModel
+                {
+                    Id = x.Id,
+                    Title = x.Title,
+                    Slug = x.Slug,
+                    Author = $"{x.Author.Name} ({x.Author.Email})",
+                    Category = x.Category.Name,
+                    LastUpdateDate = x.LastUpdateDate.ToString("dd/MM/yyyy")
+                })
+                .ToListAsync();
+
+            return Ok(new ResultViewModel<List<ListPostsViewModel>>(posts));
+        }
+    }
+}
---
+++ ViewModels/Posts/ListPostsViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace BlogAspNet.ViewModels.Posts
+{
+    public class ListPostsViewModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Slug { get; set; }
+        public string LastUpdateDate { get; set; }
+        public string Category { get; set; }
+        public string Author { get; set; }
+    }
+}
