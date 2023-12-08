8703c766c5eded0e91df3f8fac462dccdbc290ed
LucasApalosqui <lucas@apalosqui@gmail.com>
2023-12-08 17:11:32 -0300

    :sparkles: feat(performance): Pagination

--- Controllers/PostController.cs
+++ Controllers/PostController.cs
@@ -11,24 +11,37 @@ namespace BlogAspNet.Controllers
     public class PostController : ControllerBase
     {
         [HttpGet("v1/posts")]
-        public async Task<IActionResult> GetAsync([FromServices] DataContext context)
+        public async Task<IActionResult> GetAsync(
+            [FromServices] DataContext context,
+            [FromQuery]int page = 1, 
+            [FromQuery] int pageSize = 25)
         {
+            var count = await context.Posts.CountAsync();
             var posts = await context.Posts
                 .AsNoTracking()
                 .Include(x => x.Category)
                 .Include(x => x.Author)
-                //.Select(x => new ListPostsViewModel
-                //{
-                //    Id = x.Id,
-                //    Title = x.Title,
-                //    Slug = x.Slug,
-                //    Author = $"{x.Author.Name} ({x.Author.Email})",
-                //    Category = x.Category.Name,
-                //    LastUpdateDate = x.LastUpdateDate.ToString("dd/MM/yyyy")
-                //})
+                .Select(x => new ListPostsViewModel
+                {
+                    Id = x.Id,
+                    Title = x.Title,
+                    Slug = x.Slug,
+                    Author = $"{x.Author.Name} ({x.Author.Email})",
+                    Category = x.Category.Name,
+                    LastUpdateDate = x.LastUpdateDate
+                })
+                .Skip(page * pageSize)
+                .Take(pageSize)
+                .OrderByDescending(x => x.LastUpdateDate)
                 .ToListAsync();
 
-            return Ok(new ResultViewModel<List<Post>>(posts));
+            return Ok(new ResultViewModel<dynamic>(new
+            {
+                total = count,
+                page,
+                pageSize,
+                posts
+            }));
         }
     }
 }
--- ViewModels/Posts/ListPostsViewModel.cs
+++ ViewModels/Posts/ListPostsViewModel.cs
@@ -5,7 +5,7 @@
         public int Id { get; set; }
         public string Title { get; set; }
         public string Slug { get; set; }
-        public string LastUpdateDate { get; set; }
+        public DateTime LastUpdateDate { get; set; }
         public string Category { get; set; }
         public string Author { get; set; }
     }
